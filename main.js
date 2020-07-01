89436cbc9829fc3ef16b6b93ee456213bc2e31b7
favour vivian woka <vivianemma97@gmail.com>
2020-07-01 20:53:33 +0100

    creat getBudgetAmount function, addExpenses function, displayExp function, calcExpenses function and updateBalance function.

--- main.js
+++ main.js
@@ -12,3 +12,129 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+const amountInput = document.getElementById("number");
+const addForm = document.getElementById("addForm");
+const budgetAmount = document.getElementById("budgetAmount");
+const balanceAmount = document.getElementById("balanceAmount");
+const expName = document.getElementById("expName");
+const expNumber = document.getElementById("expNumber");
+const expForm = document.getElementById("expForm");
+const expensesAmount = document.getElementById("expensesAmount");
+const expValue = document.getElementById("expValue");
+const displayExpenses = document.getElementById("displayExpenses");
+let id = 0;
+const details = [];
+
+function getBudgetAmount(amount) {
+  if (!amount) {
+    amountInput.style.border = "1px solid #b80c09";
+    amountInput.placeholder = "input can not be empty";
+    amountInput.style.color = "#b80c09";
+    setTimeout(() => {
+      amountInput.style.color = "#495057";
+      amountInput.style.border = "1px solid gray";
+    }, 3000);
+  } else {
+    budgetAmount.innerText = amount;
+    balanceAmount.innerText = amount;
+    expForm.style.display = "block";
+    addForm.style.display = "none";
+    amountInput.value = "";
+  }
+}
+
+function addExpenses(name, number) {
+  if (!name.length || !number.length) {
+    expName.style.border = "1px solid #b80c09";
+    expName.placeholder = "input can not be empty";
+    expName.style.color = "#b80c09";
+
+    expNumber.style.border = "1px solid #b80c09";
+    expNumber.placeholder = "input can not be empty";
+    expNumber.style.color = "#b80c09";
+
+    setTimeout(() => {
+      expName.style.color = "#495057";
+      expName.style.border = "1px solid gray";
+      expName.placeholder = "input can not be empty";
+
+      expNumber.placeholder = "input can not be empty";
+      expNumber.style.border = "1px solid gray";
+      expNumber.style.color = "#495057";
+    }, 3000);
+  } else {
+    const userExp = {
+      id: id,
+      name: name,
+      number: parseInt(number),
+    };
+    details.push(userExp);
+    displayExp(details);
+    id++;
+    expName.value = "";
+    expNumber.value = "";
+  }
+}
+
+function displayExp(details) {
+  expValue.innerHTML = null;
+  for (i = 0; i < details.length; i++) {
+    expValue.innerHTML += `
+    <div class="expValue" id="${details[i].id}">
+      <div id="expTitleName" class="exp"><p>${details[i].name}</p></div>
+      <div id="expValueAmount" class="exp"><p> <span>$ </span> ${details[i].number}</p></div>
+      <div id="edite_delete">
+        <p>
+          <span> <img src="image/edit.svg" width="15" alt="" onclick="editExpDetails(${details[i].id})" /></span> 
+          <span><img src="image/trash.svg" width="15" alt="" onclick="deleteExpDetails(${details[i].id})"/></span>
+        </p>
+      </div>
+    </div>
+  `;
+  }
+  displayExpenses.style.display = "block";
+  calcExpenses();
+}
+
+function calcExpenses() {
+  let totalExp = 0;
+  for (i = 0; i < details.length; i++) {
+    totalExp = details[i].number + totalExp;
+  }
+  expensesAmount.innerText = totalExp;
+  updateBalance();
+}
+
+function updateBalance() {
+  balanceAmount.innerText =
+    parseInt(budgetAmount.innerText) - parseInt(expensesAmount.innerText);
+}
+
+function deleteExpDetails(id) {
+  console.log(details);
+  // console.log(id);
+  // details.splice(id, 1);
+  // console.log(details.splice(id, 1));
+  for (i = 0; i < details.length; i++) {
+    details.splice(1, i);
+    console.log(i);
+    console.log(id);
+    console.log(details.splice(1, i));
+  }
+  displayExp(details);
+}
+
+function editExpDetails(id) {
+  console.log(id);
+}
+
+expForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addExpenses(expName.value, expNumber.value);
+});
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getBudgetAmount(amountInput.value);
+});
